b5cfe45ab7af3e670c0475f755c6dfa886f76340
Mahov Sven <empenn54@gmail.com>
2017-10-17 19:57:52 -0500

    Finished Options Menu Look

--- files/scripts/loader.js
+++ files/scripts/loader.js
@@ -15,4 +15,28 @@ class Loader{
 			callback(html);
 		});
 	}
+	
+	static loadServantFile(id, callback){
+		this.loadFile(`files/servants/servant${id}.svt`, (file) => {
+			const content = file.replace(/\t+/g, "");
+			const lines = content.split("\n");
+			
+			const servant = {};
+			servant["ID"] = id;
+			for(const line of lines){
+				const parts = line.split(":");
+				const property = parts[0];
+				const value = parts[1];
+				const numValue = +value;
+				if(isNaN(numValue)){
+					// Value is a string
+					servant[property] = value;
+				}else{
+					servant[property] = numValue;
+				}
+			}
+			
+			callback(servant);
+		})
+	}
 }
--- files/scripts/main.js
+++ files/scripts/main.js
@@ -1,5 +1,9 @@
 function globals(){}
 globals.banners = 2;
+globals.servants = 1;
+
+const servants = [];
+const measures = new Set();
 
 function setBanner(bannerID){
 	document.body.style.backgroundImage = "url(files/images/banners/banner" + bannerID + ".png)";
@@ -9,6 +13,46 @@ function setBanner(bannerID){
 	document.body.style.backgroundSize = "cover"
 }
 
-$(document).ready(function () {
+function handleOptionClick(e){
+	$(".Option").removeClass("OptionActive");
+	$(this).addClass("OptionActive");
+	
+	const id = $(this).attr("id");
+	
+	switch(id){
+	case "STATS":
+		clearContentSpace();
+		generateStatsTable();
+		break;
+	}
+}
+
+function clearContentSpace(){
+	$("#CONTENT_SPACE").empty();
+}
+
+function generateStatsTable(){
+	const tableBase = $("<table>");
+	const div = $("<div>");
+	div.attr("background-color", "black");
+	div.attr("height", "10px");
+	div.attr("width", "10px");
+	$("#CONTENT_SPACE").append(div);
+}
+
+$(document).ready(() => {
 	setBanner(randomInt(1, globals.banners));
+	
+	$(".Option").click(handleOptionClick);
+	
+	for(let id = 1; id <= globals.servants; id++){
+		Loader.loadServantFile(id, (servant) => {
+			servants.push(servant);
+			for(const measure in servant){
+				measures.add(measure);
+			}
+		});
+	}
+	console.log(servants);
+	console.log(measures);
 });
